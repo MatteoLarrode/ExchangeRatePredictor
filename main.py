949951e2a1290a1473cc406ed8baf312f66d29ca
MatteoLarrode <matteo.larrode@orange.fr>
2022-09-10 21:19:00 -0400

    first API calls

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import config
+import requests
+from pprint import pprint
 
-#access API key: config.api_key
+from config import api_key
+
+#call latest exchange rates
+
+url_latest="https://api.apilayer.com/fixer/latest?symbols=GBP&base=USD"
+payload = {}
+headers= {
+  "apikey": api_key
+}
+
+response_latest = requests.request("GET", url_latest, headers=headers, data = payload)
+
+status_code = response_latest.status_code
+result_latest = response_latest.text
+
+print(result_latest)
+
+with open("response.txt", "w") as f:
+    f.write(result_latest)
